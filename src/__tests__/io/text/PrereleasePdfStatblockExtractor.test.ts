4d9464f9c8eb2da381861d0b0dd339809a9f7ac5
Scott Tomaszewski <scottTomaszewski@gmail.com>
2025-06-20 10:54:12 -0400

    Updates with full adventure text

--- src/__tests__/io/text/PrereleasePdfStatblockExtractor.test.ts
+++ src/__tests__/io/text/PrereleasePdfStatblockExtractor.test.ts
@@ -6,7 +6,7 @@ describe("PrereleasePdfStatblockExtractor", () => {
     it("should extract the statblocks from the prerelease pdf text", () => {
         const text = fs.readFileSync(path.join(__dirname, "../../data/pdf/prerelease-pdf/road_to_broadhurst.txt"), "utf-8");
         const statblocks = PrereleasePdfStatblockExtractor.extractStatblockText(text);
-        expect(statblocks).toHaveLength(4);
+        expect(statblocks).toHaveLength(8);
 
         const names = statblocks.map((s: string) => s.split("\n")[0]);
         expect(names).toEqual([
@@ -14,6 +14,10 @@ describe("PrereleasePdfStatblockExtractor", () => {
             "R ADENWIGHT SWIFTPAW LEVEL 1 M INION HARRIER",
             "R ADENWIGHT R EDEYE LEVEL 1 M INION ARTILLERY",
             "R ADENWIGHT M AESTRO LEVEL 1 LEADER",
+            "G OBLIN SNIPER LEVEL 1 M INION ARTILLERY",
+            "G OBLIN ASSASSIN LEVEL 1 HORDE AMBUSHER",
+            "G OBLIN STINKER LEVEL 1 HORDE C ONTROLLER",
+            "G OBLIN WARRIOR LEVEL 1 HORDE HARRIER"
         ]);
     });
 }); 
